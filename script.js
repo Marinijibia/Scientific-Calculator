12e98a8a518a15135bd3bf2d7ca5e526e1c26f4a
Umar falalu <marinijibiaa@gmail.com>
2024-05-18 17:11:26 -0700

    Pushing the cal functionality

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ const getMonthName = (mothIndex) => {
 
   return months[mothIndex];
 };
+
+// getting the tim and date
 const startTime = () => {
   const date = new Date();
   let hour = date.getHours();
@@ -52,4 +54,87 @@ const startTime = () => {
     "div.date"
   ).innerHTML = `${padDay} ${monthName} ${year}`;
   setTimeout(startTime, 200);
+};
+
+//setting screen
+
+let screen = document.querySelector(".srn1");
+
+const press = (numValue) => {
+  if (screen.value == 0) {
+    screen.value = "";
+  }
+  screen.value += numValue;
+};
+
+//screen clearing
+const clearScreen = (val) => (screen.value = val);
+
+//Del Key
+const delKey = () => {
+  const clear = screen;
+  screen.value = clear.value.substring(0, clear.value.length - 1);
+};
+
+//calculate function
+const calFunc = () => {
+  try {
+    clearScreen2(eval(screen.value));
+  } catch {
+    clearScreen2("Bad Expression");
+  }
+};
+
+let screen2 = document.querySelector("#screen2");
+const clearScreen2 = (val) => {
+  screen2.textContent = val ? val : "Ans";
+};
+
+// Calculating sin, cos and tan
+const sine = () => (screen.value = Math.sin((screen.value * Math.PI) / 180));
+const cose = () => (screen.value = Math.cos((screen.value * Math.PI) / 180));
+const taan = () => (screen.value = Math.tan((screen.value * Math.PI) / 180));
+
+// Calculating Asin, acos and atan
+const invSin = () => (screen.value = Math.asin(screen.value) * (180 / Math.PI));
+
+const invCos = () => {
+  if (screen.value >= -1 && screen.value <= 1) {
+    screen.value = Math.acos(screen.value) * (180 / Math.PI);
+  } else {
+    screen.value = "value must be between -1 and 1";
+  }
+};
+
+const invTan = () => {
+  if (screen.value >= -1 && screen.value <= 1) {
+    screen.value = Math.atan(screen.value) * (180 / Math.PI);
+  } else {
+    screen.value = "value must be between -1 and 1";
+  }
+};
+
+//calculating ln amd Log
+const ln = () => (screen.value = Math.log(screen.value));
+const log = () => (screen.value = Math.log10(screen.value));
+
+//calculating %
+const percent = () => (screen.value = screen.value / 100);
+
+//calculating square, cube and sqrt
+const square = () => (screen.value = Math.pow(screen.value, 2));
+const cube = () => (screen.value = Math.pow(screen.value, 3));
+const sqrt = () => (screen.value = Math.sqrt(screen.value));
+
+//plusMinus function
+const plusMinus = () => {
+  let x = screen;
+  let y = parseFloat(x.value);
+  y *= -1;
+  x.value = y;
+};
+
+//Exp function
+const exp = () => {
+  screen.value ? (screen.value = Math.pow(10, screen.value)) : screen.value;
 };
